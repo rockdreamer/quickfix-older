c7cbcaa317a0034fe3fa7fb30dbf177d68ea2471
Claudio Bantaloukas <rockdreamer@gmail.com>
2009-08-05 16:01:26 +0200

    correction in example building

---
+++ src/config-base.h
@@ -0,0 +1,19 @@
+#ifndef QF_CONFIG_H
+#define QF_CONFIG_H
+#cmakedefine ENABLE_CALLSTACK
+#cmakedefine HAVE_STLPORT
+#cmakedefine HAVE_JAVA
+#cmakedefine HAVE_MYSQL
+#cmakedefine HAVE_POSTGRESQL
+#cmakedefine HAVE_ODBC
+#cmakedefine HAVE_LIBXML
+#cmakedefine HAVE_FTIME
+#cmakedefine USING_STREAMS
+#cmakedefine socklen_t
+#cmakedefine TERMINATE_IN_STD
+#cmakedefine TYPEINFO_IN_STD
+#cmakedefine GETHOSTBYNAME_R_INPUTS_RESULT
+#cmakedefine ENABLE_NEW_ALLOCATOR
+#endif
+
+
